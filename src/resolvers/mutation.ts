ede4b3350e74e68e2d25f6ae902372e3a6db726b
Shunsuke Hioki <iron.kicka624@gmail.com>
2021-11-29 15:04:26 +0900

    destructure context

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,8 +1,8 @@
 import {MutationResolvers} from "../generated/graphql";
 
 const Mutation:MutationResolvers = {
-    newNote:async(parent,args,context)=>{
-        return await context.models.Note.create({
+    newNote:async(parent,args,{models})=>{
+        return await models.Note.create({
             content:args.content,
             author:'Adam Scott'
         })
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,11 +1,11 @@
 import {QueryResolvers} from "../generated/graphql";
 
 const Query:QueryResolvers ={
-    notes:async(parent,args,context)=>{
-        return context.models.Note.find();
+    notes:async(parent,args,{models})=>{
+        return models.Note.find();
     },
-    note:async(parent,args,context)=>{
-        return context.models.Note.findById(args.id)
+    note:async(parent,args,{models})=>{
+        return models.Note.findById(args.id)
     }
 }
 
